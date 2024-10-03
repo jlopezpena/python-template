03a1dc76ee6b9ee87f33c1cb9802f28b1ea200c3
Javier López Peña <javier.lopez@oakam.com>
2024-10-03 15:57:47 +0000

    reformt

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,9 +1,10 @@
 # Verify that our module can be installed...
 
+
 def test_import():
     try:
         import python_template
+
         assert True
     except ImportError:
         assert False
-        
