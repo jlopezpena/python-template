f32f5a9d89f2bfa4c34fad43289d68f3bef67b78
Javier López Peña <javier.lopez@oakam.com>
2024-10-03 15:31:44 +0000

    set coverage threshold to 100

---
+++ tests/test_import.py
@@ -0,0 +1,9 @@
+# Verify that our module can be installed...
+
+def test_import():
+    try:
+        import python_template
+        assert True
+    except ImportError:
+        assert False
+        
