01d928293573423a1aa3bb357ec5d39491feaec5
Javier López Peña <jlopezpena@users.noreply.github.com>
2024-10-21 10:26:42 +0100

    tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,3 +1,7 @@
+from jlp.python_template.hello import say_hi
+
+
 def test_dummy() -> None:
     """Dummy test for checking that CI works"""
-    assert True
+    greet = say_hi("world")
+    assert greet == "Hi, world"
