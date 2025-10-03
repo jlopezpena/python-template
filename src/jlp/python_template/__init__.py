6c093f47f8862e5f800110729ec94c06f3802bb1
Javier López Peña <jlopezpena@users.noreply.github.com>
2025-10-03 13:11:59 +0200

    fix: remove duplicate function and sort imports

--- src/jlp/python_template/__init__.py
+++ src/jlp/python_template/__init__.py
@@ -6,7 +6,7 @@ TEMPLATE: values like project name and version will be parameterized by Copier i
 __all__: list[str] = []
 
 try:  # pragma: no cover - defensive import pattern
-    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+    from importlib.metadata import PackageNotFoundError, version  # type: ignore
     try:
         __version__ = version("python-template")  # TEMPLATE: {{ repo_name }}
     except PackageNotFoundError:  # pragma: no cover
--- src/jlp/python_template/hello.py
+++ src/jlp/python_template/hello.py
@@ -4,5 +4,3 @@ def say_hi(name: str) -> str:
     TEMPLATE: Module may be optional in derived projects.
     """
     return f"Hi, {name}"
-def say_hi(name: str) -> str:
-    return f"Hi, {name}"
